9f34378fa24be017e8d53236622a859a4f1c7390
AhmedMazroua <126991045+AhmedMazroua@users.noreply.github.com>
2024-02-12 19:39:33 -0500

    Update todo_list.py

--- todo_list.py
+++ todo_list.py
@@ -1,18 +1,24 @@
+#initialized class
 class TodoList:
+    
+    #default parameter
     def __init__(self):
         self.tasks = []
 
+    #add task method
     def add_task(self, task):
         self.tasks.append(task)
         print(f"Task '{task}' added successfully!")
 
+    #remove task method
     def remove_task(self, task):
         if task in self.tasks:
             self.tasks.remove(task)
             print(f"Task '{task}' removed successfully!")
         else:
             print(f"Task '{task}' not found in the list.")
-
+   
+    #view task method
     def view_tasks(self):
         if self.tasks:
             print("Todo List:")
@@ -21,10 +27,11 @@ class TodoList:
         else:
             print("Todo List is empty.")
 
-
+#main body of todo list
 def main():
     todo_list = TodoList()
 
+    #flag to run program while boolean is met
     while True:
         print("\nTodo List Manager")
         print("1. Add Task")
@@ -32,6 +39,7 @@ def main():
         print("3. View Tasks")
         print("4. Exit")
 
+        #requests input from user to then check conditions 1, 2, 3, or 4
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -48,6 +56,6 @@ def main():
         else:
             print("Invalid choice. Please enter a number between 1 and 4.")
 
-
+#ensures script is only run in this file and not on import
 if __name__ == "__main__":
     main()
